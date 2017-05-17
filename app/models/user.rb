ca26fa38aa8899c3171fd7067dda222d9e2856cb
John Wujciak <jmw2257@gmail.com>
2017-05-17 01:06:56 +0000

    Added email format validation with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ActiveRecord::Base
     presence: true, 
     uniqueness: { case_sensitive: false },
     length: { minimum: 2, maximum: 50 }
-    
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, 
     presence: true, 
     uniqueness: { case_sensitive: false },
-    length: { minimum: 7, maximum: 50 }
+    length: { minimum: 7, maximum: 50 },
+    format: { with: VALID_EMAIL_REGEX }
 end
